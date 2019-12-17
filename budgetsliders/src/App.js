05ea301d66fa2fdce374017d20ca4fb6502119c5
IanB13 <48295744+IanB13@users.noreply.github.com>
2019-12-17 16:01:00 -0700

    Added add remove functionality

--- budgetsliders/src/App.js
+++ budgetsliders/src/App.js
@@ -3,26 +3,23 @@ import './App.css';
 const dummylist =[
   {
     id: 0,
-    budget: 1000,
+    total: 1000,
     cost: 50,
     quantity: 20
   },{
     id: 1,
-    budget: 1000,
+    total: 1000,
     cost: 50,
     quantity: 20
   },{
     id: 2,
-    budget: 1000,
+    total: 1000,
     cost: 50,
     quantity: 20
   }
 ]
 
-
 const App = () => {
-
-  
   return (
     <SliderContainer />
   );
@@ -30,17 +27,62 @@ const App = () => {
 
 const SliderContainer =(props) =>{
   // eslint-disable-next-line 
-  const [budget,changebudget] =useState(3000)
-  const [sliders, changesliders] =useState(dummylist)
+  const [budget,changeBudget] =useState(3000)
+  const [sliders, changeSliders] =useState(dummylist)
   
   return (
     <div className ="App">
-        <h1>{budget} </h1>
-        <div>Add + remove </div>
-        <Sliders sliders = {sliders} changesliders = {changesliders} totalBudget={budget}/>
+        <BudgetInput budget = {budget} changeBudget = {changeBudget}/>
+        <AddRemoveSlider sliders = {sliders} changeSliders = {changeSliders} t/>
+        <Sliders sliders = {sliders} changesliders = {changeSliders} totalBudget={budget}/>
     </div>
   );
 }
+const BudgetInput =({budget,changeBudget}) =>{
+
+
+  const budgetUpdate = (event)=>{
+    changeBudget(event.target.value)
+  }
+
+  return(
+    <input onChange = {budgetUpdate} value ={budget} />
+      
+  )
+
+}
+
+const AddRemoveSlider = ({sliders,changeSliders}) =>{
+
+  const AddSlider =()=>{
+    console.log("add")
+    const id = sliders.length
+    const addedSliderArray = [...sliders,{
+      id,
+      total: 0,
+      cost: 0,
+      quantity: 0
+    }]
+    console.log(addedSliderArray)
+    changeSliders(addedSliderArray)
+
+  }
+  const RemoveSlider =()=>{
+    console.log("remove")
+    const removedSliderArray = sliders.slice(0,-1)
+   changeSliders(removedSliderArray)
+
+  }
+return(
+  <div>
+    <button onClick ={AddSlider}>Add </button>
+    <button onClick ={RemoveSlider}>Remove</button>
+  </div>
+
+)
+}
+
+
 
 const Sliders =(props) =>{
 const sliderArray = props.sliders;
@@ -50,7 +92,7 @@ const totalBudget =props.totalBudget;
     <div>
       {sliderArray.map(slider => { 
         return( <Slider 
-          sliderBudget ={slider.budget} 
+          sliderBudget ={slider.total} 
           sliderId={slider.id} 
           key = {slider.id}
           changesliders = {changesliders} 
@@ -69,27 +111,26 @@ const sliderArray = props.sliderArray;
 const sliderId = props.sliderId;
 const totalBudget = props.totalBudget;
 
-  const handleSliderChange = (event) => {
+  const handleTotalChange = (event) => {
 
     //function that changes values based on the array
     
     //get average
     //TODO: add lock functonality
-
+    // add lock + figure out functions
     const newAmount = (totalBudget-event.target.value)/(sliderArray.length-1)
     const newArray = sliderArray.map(slider =>{
-     // console.log(`${sliderId} == ${slider.id}`)
       if(sliderId === slider.id){
         const changedSlider = {
           id: slider.id,
-          budget: event.target.value
+          total: event.target.value
         };
         return changedSlider
       }
       else{
         const changedSlider = {
           id: slider.id,
-          budget: newAmount
+          total: newAmount
         };
         return changedSlider
       }
@@ -99,15 +140,18 @@ const totalBudget = props.totalBudget;
   
   return(
     <div>
-    <input value = {sliderArray[sliderId].budget} 
+      <input value = {sliderArray[sliderId].quantity} 
+      //need to add inputs
+      /> quantity *
+      <input value = {sliderArray[sliderId].cost} 
+      
+      />cost = 
+    <input value = {sliderArray[sliderId].total}
       id = {sliderId}
-      onChange ={handleSliderChange} />
+      onChange ={handleTotalChange} />
+      total
   </div>
   )
 }
-
-
-
-
 
 export default App;
---
+++ budgetsliders/todo.md
@@ -0,0 +1,4 @@
+#to DO
+Add:
+quantity
+cost
